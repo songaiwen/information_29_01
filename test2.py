354ce41cc9c81076fc5e9b71e07770c187776c22
经理 <1870961767@qq.com>
2018-05-24 10:50:19 +0800

    公共方法

---
+++ test2.py
@@ -0,0 +1,23 @@
+# 字符串拼接
+result = "hello" + "world"
+print(result)
+
+# 列表合并
+result = [1,2,] + [3,4]
+print(result)
+result = (5,6) + (7,8)
+print(result)
+
+# 复制
+result = "A" * 5
+print(result)
+
+result = [12, 36] * 3
+print(result)
+
+result = ("星星", "月亮") * 4
+print(result)
+
+# in 判断参数是否在指定对象里面
+result = "a" in "add"
+print(result)
